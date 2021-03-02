06e64e3d5395ca23a05d8e229fc0635b200d4f11
Antti Kylmanen <anttikk84@hotmail.com>
2021-03-02 10:16:53 +0200

    test1.3 reminderslist

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 const express = require('express')
 const app = express()
 
-let reminders = [
-  {
-    id: 1,
-    content: 'HTML on helppoa',
-    date: '2017-12-10T17:30:31.098Z',
-    important: true
-  },
-  {
-    id: 2,
-    content: 'Selain pystyy suorittamaan vain javascriptiä',
-    date: '2017-12-10T18:39:34.091Z',
-    important: false
-  },
-  {
-    id: 3,
-    content: 'HTTP-protokollan tärkeimmät metodit ovat GET ja POST',
-    date: '2017-12-10T19:20:14.298Z',
-    important: true
-  }
-]
+let reminders = {
+  "reminders": [
+    {
+      "name": "Buy some eggs",
+      "timestamp": "2021-11-10T13:00:00.141Z",
+      "id": 1
+    },
+    {
+      "name": "Make an omelette",
+      "timestamp": "2021-11-11T08:00:00.141Z",
+      "id": 2
+    },
+    {
+      "name": "Wash dishes",
+      "timestamp": "2021-11-11T09:00:00.000Z",
+      "id": 3
+    },
+    {
+      "name": "Buy more eggs",
+      "timestamp": "2021-11-11T13:00:00.000Z",
+      "id": 4
+    }
+  ]
+}
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
