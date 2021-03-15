962877bd9f90029a881014bd1d974ee04a9f664e
Antti Kylmanen <anttikk84@hotmail.com>
2021-03-15 13:30:21 +0200

    initiate app.

--- index.js
+++ index.js
@@ -107,12 +107,12 @@ app.post('/api/reminders/', (request, response) =>{
   response.json(reminder)
 })
 
-const error = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-app.use(error)
+//const error = (request, response) => {
+//  response.status(404).send({ error: 'unknown endpoint' })
+//}
+//app.use(error)
 
-const PORT = process.env.PORT 
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
