3b7957087cd8b2736351859813a0de9989d898e9
Antti Kylmanen <anttikk84@hotmail.com>
2021-03-17 21:09:42 +0200

    ex3.12

--- index.js
+++ index.js
@@ -9,43 +9,6 @@ app.use(bodyParser.json())
 app.use(express.static('build'))
 
 
-
-
-/*
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
-app.use(requestLogger)
-*/
-/*
-let reminders = {
-  "reminders": [
-    {
-      "name": "Buy some eggs",
-      "timestamp": "2021-11-10T13:00:00.141Z",
-      "id": 1
-    },
-    {
-      "name": "Make an omelette",
-      "timestamp": "2021-11-11T08:00:00.141Z",
-      "id": 2
-    },
-    {
-      "name": "Wash dishes",
-      "timestamp": "2021-11-11T09:00:00.000Z",
-      "id": 3
-    },
-    {
-      "name": "Buy more eggs",
-      "timestamp": "2021-11-11T13:00:00.000Z",
-      "id": 4
-    }
-  ]
-}*/
 const formatReminder = (reminder) => {
   return {
     name: reminder.name,
@@ -53,10 +16,6 @@ const formatReminder = (reminder) => {
     id: reminder._id
   }
 }
-
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
 
 app.get('/api/reminders', (request, response) => {
   Reminder
@@ -68,15 +27,20 @@ app.get('/api/reminders', (request, response) => {
 })
 
 app.get('/api/reminders/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const reminder = reminders.reminders.find(reminder => reminder.id === id)
-
-  if (reminder) {
-    response.json(reminder) 
-  } else {
-    response.status(404).end()
-   
-  }
+  Reminder
+    .findById(request.params.id)
+    .then(reminder => {
+      if (reminder) {
+        response.json(formatReminder(reminder))
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(400).send({ error: 'malformatted id' })
+    })
+  
 })
 
 app.delete('/api/reminders/:id', (request, response) =>{
@@ -89,25 +53,16 @@ app.delete('/api/reminders/:id', (request, response) =>{
       response.status(400).send({ error: 'malformatted id' })
     })
   
-  /*
-  const id = Number(request.params.id)
-  reminders.reminders = reminders.reminders.filter(reminder => reminder.id !== id)
-  response.status(204).end()
-  */
+  
 })
 
 /*
-const generateId = () => {
-  const id = Math.floor(Math.random()*10000000)
-  return id
-}
-
 const isInArray = (props) => {
   const names= reminders.reminders.map(name => name.name)
   const verdict = names.includes(props)
   return verdict
-}
-*/
+}*/
+
 
 app.post('/api/reminders/', (request, response) =>{
   const body = request.body
@@ -115,22 +70,20 @@ app.post('/api/reminders/', (request, response) =>{
   if (body.name === undefined) {
     return response.status(400).json({error: 'content missing'})
   }
-/* 
+
   if (body.name.trim().length === 0 || body.timestamp.trim().length === 0){
     return response.status(400).json({error: 'content missing!'})
   }
   
-  if (isInArray(body.name)){
+  /*if (isInArray(body.name)){
     return response.status(400).json({error: 'name must be unique'})
-  }
-*/
+  }*/
+
   const reminder = new Reminder({
     name: body.name,
-    timestamp: body.timestamp,
-    //id: generateId()
+    timestamp: body.timestamp
   })
 
-  //reminders.reminders= reminders.reminders.concat(reminder)
   reminder
     .save()
     .then(savedReminder => {
@@ -138,11 +91,6 @@ app.post('/api/reminders/', (request, response) =>{
     })
   
 })
-
-//const error = (request, response) => {
-//  response.status(404).send({ error: 'unknown endpoint' })
-//}
-//app.use(error)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
--- mongo.js
+++ mongo.js
@@ -1,41 +1,21 @@
 const mongoose = require('mongoose')
 
-// Replace with the URL of your own database. Do not store the password on GitLab!
 const url = process.env.MONGODB_URI
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true})
 
 const reminderSchema = new mongoose.Schema({
     name: String,
-    //timestamp: Date
     timestamp: String
 })
 
 const Reminder = mongoose.model('Reminder', reminderSchema)
-  
-/*
-if (process.argv.length<=4) {
-    //console.log('anna muistutus ja ajankohta')
-    //process.exit(1)
-    Reminder
-        .find({})
-        .then(reminders => {
-        reminders.forEach(reminder => {
-          console.log(reminder)
-        })
-        mongoose.connection.close()
-        process.exit(1)
-    })
-}*/
-
-
   
 const name = process.argv[2]
 const time = process.argv[3]
 
 const reminder = new Reminder({
   name: name,
-  //timestamp: new Date(time)
   timestamp: time
 })
 
@@ -45,7 +25,6 @@ process.argv.length<=3?
         console.log(rmndr)
     })
         mongoose.connection.close()
-        //process.exit(1)
     }):
     reminder
         .save()
@@ -53,23 +32,3 @@ process.argv.length<=3?
         console.log(`adding Reminder ${reminder.name} at ${reminder.timestamp} to the reminder database`)
         mongoose.connection.close()
   })
-
-
-/*if (process.argv.length<=3) {
-    Reminder.find({}).then(result =>{
-        result.forEach(rmndr =>{
-            console.log(rmndr)
-        })
-            mongoose.connection.close()
-            //process.exit(1)
-    })
-
-}
-if(process.argv.length >= 4){
-    reminder
-        .save()
-        .then(response => {
-        console.log(`adding Reminder ${reminder.name} at ${reminder.timestamp} to the reminder database`)
-        mongoose.connection.close()
-  })
-}*/
